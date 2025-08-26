bd8752bccec47df73b264546905b36e9536b2f4f
Cam-Wang-Sphere <cam.wang@thesphere.com>
2025-08-26 13:24:27 -0700

    Adding test3 with javascript source

---
+++ Test3/script.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+    renderFighters(4);
+};
+
+function renderFighters(numFighters)    {
+    let baseName = 'Rot';
+
+    for (let index = 0; index < numFighters; index++) {
+        let realIndex = index % 4;
+        let fullName = baseName + realIndex.toString();
+
+        let Anchor = document.querySelector("a-entity[name='" + fullName + "']");
+
+        let Model = document.createElement('a-entity');
+        Model.setAttribute('gltf-model', './assets/tie/scene.gltf');
+        Model.setAttribute('scale','0.6 0.6 0.6');
+        
+        Model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        Anchor.appendChild(Model);
+    }
+};
