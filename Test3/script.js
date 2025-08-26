c493066eb398363f7b19053cba1d96724af7d500
Cam-Wang-Sphere <cam.wang@thesphere.com>
2025-08-26 13:44:23 -0700

    just 0 indexed myself

--- Test3/script.js
+++ Test3/script.js
@@ -1,16 +1,16 @@
 window.onload = () => {
-    renderFighters(4);
+    renderFighters(6);
 };
 
 function renderFighters(numFighters)    {
     let baseName = 'Rot';
 
-    for (let index = 0; index < numFighters; index++) {
+    for (let index = 1; index <= numFighters; index++) {
         let realIndex = index % 4;
         let fullName = baseName + realIndex.toString();
 
         console.log("Looking for entity with name: " + fullName);
-        
+
         let Anchor = document.querySelector("a-entity[name='" + fullName + "']");
 
         let Model = document.createElement('a-entity');
