43827a91510b5e92f11f375fece05556ac1b9d34
Cam-Wang-Sphere <cam.wang@thesphere.com>
2025-08-26 14:10:03 -0700

    adding on click handler

--- Test3/script.js
+++ Test3/script.js
@@ -1,5 +1,9 @@
 window.onload = () => {
-    renderFighters(6);
+    // renderFighters(6);
+
+    window.addEventListener("click", (event) => {
+        spawnNewFighter();
+    });
 };
 
 var count = 0
@@ -30,3 +34,28 @@ function renderFighters(numFighters)    {
         count++;
     }
 };
+
+function spawnNewFighter()
+{
+    let baseName = 'Rot';
+    let realIndex = (count % 4) + 1;
+    let fullName = baseName + realIndex.toString();
+
+    console.log("Looking for entity with name: " + fullName);
+
+    let Anchor = document.querySelector("a-entity[name='" + fullName + "']");
+
+    let Model = document.createElement('a-entity');
+    Model.setAttribute('gltf-model', './assets/tie/scene.gltf');
+    Model.setAttribute('scale','0.6 0.6 0.6');
+    
+    Model.addEventListener('loaded', () => {
+        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+    });
+
+    Anchor.appendChild(Model);
+    console.log("Addeda fighter to scene");
+
+    console.log("Count" + count.toString());
+    count++;
+}
