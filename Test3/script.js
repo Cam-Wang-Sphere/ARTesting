7ccc8cec93af17f92709ce80d4d5bd9141a168c7
Cam-Wang-Sphere <cam.wang@thesphere.com>
2025-08-26 14:19:13 -0700

    a whole bunch of updates

--- Test3/script.js
+++ Test3/script.js
@@ -1,39 +1,12 @@
 window.onload = () => {
-    // renderFighters(6);
-
-    window.addEventListener("click", (event) => {
+    const button = document.querySelector('button[data-action="change"]');
+    button.innerText = 'Add';
+    button.addEventListener('click', function () {
         spawnNewFighter();
     });
 };
 
 var count = 0
-
-function renderFighters(numFighters)    {
-    let baseName = 'Rot';
-
-    for (let index = 0; index < numFighters; index++) {
-        let realIndex = (count % 4) + 1;
-        let fullName = baseName + realIndex.toString();
-
-        console.log("Looking for entity with name: " + fullName);
-
-        let Anchor = document.querySelector("a-entity[name='" + fullName + "']");
-
-        let Model = document.createElement('a-entity');
-        Model.setAttribute('gltf-model', './assets/tie/scene.gltf');
-        Model.setAttribute('scale','0.6 0.6 0.6');
-        
-        Model.addEventListener('loaded', () => {
-            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-        });
-
-        Anchor.appendChild(Model);
-        console.log("Addeda fighter to scene");
-
-        console.log("Count" + count.toString());
-        count++;
-    }
-};
 
 function spawnNewFighter()
 {
